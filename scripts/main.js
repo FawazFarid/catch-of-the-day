3ae7c10399206400e7042c55cbf274ac3ed2d3cc
Fawaz Farid <fawwazally@gmail.com>
2017-03-22 20:02:36 +0300

    feat(app components): added extra components

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,63 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var App = React.createClass({
+
+  render : function(){
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="Fresh Seafood Market" />
+        </div>
+        <Order />
+        <Inventory />
+      </div>
+    )
+  }
+});
+
+/**
+ * Header Component
+ */
+var Header = React.createClass({
+  render : function(){
+    return (
+      <header className="top">
+        <h1>Catch
+          <span className="ofThe">
+            <span className="of"> Of </span>
+            <span className="the"> The </span>
+          </span>
+          Day
+        </h1>
+        <h3 className="tagLine"><span>{this.props.tagline}</span></h3>
+      </header>
+    )
+  }
+});
+
+/**
+ * Order Component
+ */
+var Order = React.createClass({
+  render : function(){
+    return (
+      <p>Order</p>
+    )
+  }
+});
+
+/**
+ * Inventory Component
+ */
+var Inventory = React.createClass({
+  render : function(){
+    return (
+      <p>Inventory</p>
+    )
+  }
+});
+
 var StorePicker = React.createClass({
 
   render : function(){
@@ -16,4 +73,4 @@ var StorePicker = React.createClass({
 });
 
 
-ReactDOM.render(<StorePicker/>, document.querySelector('#main'));
+ReactDOM.render(<App/>, document.querySelector('#main'));
