958df8a76488e3120929fa2cdd728f9ba8312cf9
Fawaz Farid <fawwazally@gmail.com>
2017-04-18 22:49:06 +0300

    feat(store): button for adding fish to order

--- scripts/main.js
+++ scripts/main.js
@@ -54,6 +54,8 @@ var App = React.createClass({
 var Fish = React.createClass({
   render : function() {
     var details = this.props.details;
+    var isAvailable = (details.status === 'available' ? true : false);
+    var buttonText = (isAvailable ? 'Add To Order' : 'Sold Out!');
     return (
       <li className="menu-fish">
         <img src={details.image} alt={details.name} />
@@ -62,6 +64,7 @@ var Fish = React.createClass({
           <span className="price">{helpers.formatPrice(details.price)}</span>
         </h3>
         <p>{details.desc}</p>
+        <button disabled={!isAvailable} onClick={this.onButtonClick}>{buttonText}</button>
       </li>
     )
   }
