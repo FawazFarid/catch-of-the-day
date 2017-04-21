5faf283f7f96ce624e4ad89f5dd17693974c47f4
Fawaz Farid <fawwazally@gmail.com>
2017-04-21 22:30:10 +0300

    feat(order): render order list

--- scripts/main.js
+++ scripts/main.js
@@ -143,6 +143,22 @@ var Header = React.createClass({
  * Order Component
  */
 var Order = React.createClass({
+  renderOrder : function(key){
+    var fish = this.props.fishes[key];
+    var count = this.props.order[key];
+
+    if(!fish) {
+      return <li key={key}>Sorry, fish no longer available!</li>
+    }
+    return(
+      <li>
+        {count}lbs
+        {fish.name}
+        <span className="price">{helpers.formatPrice(count * fish.price)}</span>
+      </li>
+    )
+
+  },
   render : function(){
     var orderIds = Object.keys(this.props.order);
     var total = orderIds.reduce((prevTotal, key)=> {
@@ -159,6 +175,7 @@ var Order = React.createClass({
       <div className="order-wrap">
         <h2 className="order-title">Your Order</h2>
         <ul className="order">
+          {orderIds.map(this.renderOrder)}
           <li className="total">
             <strong>Total:</strong>
             {helpers.formatPrice(total)}
