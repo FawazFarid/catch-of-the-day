6fa24bdbb5d0caf7d559432b337e19338ab52e73
Fawaz Farid <fawwazally@gmail.com>
2017-05-01 21:39:24 +0300

    feat(order): animate adding and removing items from order

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var CSSTransitionGroup = require('react-addons-css-transition-group');
 var ReactRouter = require('react-router');
 var createBrowserHistory = require('history/lib/createBrowserHistory');
 var helpers = require('./helpers');
@@ -46,6 +47,12 @@ var App = React.createClass({
     this.state.order[key] = this.state.order[key] + 1 || 1;
     this.setState({ order : this.state.order });
   },
+  removeFromOrder : function(key) {
+    delete this.state.order[key];
+    this.setState({
+      order : this.state.order,
+    });
+  },
   addFish: function(fish) {
     var timestamp = (new Date()).getTime();
     // update state object
@@ -79,7 +86,7 @@ var App = React.createClass({
             {Object.keys(this.state.fishes).map(this.renderFish)}
           </ul>
         </div>
-        <Order fishes={this.state.fishes} order={this.state.order}/>
+        <Order fishes={this.state.fishes} order={this.state.order} removeFromOrder={this.removeFromOrder} />
         <Inventory addFish={this.addFish} loadSamples={this.loadSamples}
           fishes={this.state.fishes} linkState={this.linkState} removeFish={this.removeFish}
         />
@@ -181,15 +188,17 @@ var Order = React.createClass({
   renderOrder : function(key){
     var fish = this.props.fishes[key];
     var count = this.props.order[key];
+    var removeButton = <button onClick={this.props.removeFromOrder.bind(null, key)}>&times;</button>
 
     if(!fish) {
-      return <li key={key}>Sorry, fish no longer available!</li>
+      return <li key={key}>Sorry, fish no longer available! {removeButton}</li>
     }
     return(
       <li key={key}>
         {count}lbs
         {fish.name}
         <span className="price">{helpers.formatPrice(count * fish.price)}</span>
+        {removeButton}
       </li>
     )
 
@@ -209,13 +218,19 @@ var Order = React.createClass({
     return (
       <div className="order-wrap">
         <h2 className="order-title">Your Order</h2>
-        <ul className="order">
+        <CSSTransitionGroup
+          className="order"
+          component="ul"
+          transitionName="order"
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={500}
+        >
           {orderIds.map(this.renderOrder)}
           <li className="total">
             <strong>Total:</strong>
             {helpers.formatPrice(total)}
           </li>
-        </ul>
+        </CSSTransitionGroup>
       </div>
     )
   }
